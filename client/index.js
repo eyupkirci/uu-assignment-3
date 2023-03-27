dd91abf363a591eab7c5779d1bcf803821465891
eyup.kirci <eyup.kirci@finspiretech.com>
2023-03-27 19:24:04 +0200

    Authentication finished

--- client/index.js
+++ client/index.js
@@ -1,20 +1,77 @@
-let user;
+let user = JSON.parse(localStorage.getItem("user"));
 
+console.log("user", user);
+
+const headerMenu = document.getElementById("header-menu");
 const loginForm = document.getElementById("login-form");
 const registerForm = document.getElementById("register-form");
+const loginButton = document.getElementById("login-button");
+const userLi = document.getElementsByClassName("user_li");
 
-window.onload = () => {
-  console.log("window fully loaded");
+const isLoggedUser = (user) => {
+  if (user == null || user == "") {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.classList = "user_li anonymous";
+    a.setAttribute("href", "./login.html");
+    a.textContent = "Login";
+    li.appendChild(a);
+    headerMenu.appendChild(li);
+  } else {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.setAttribute("href", "#");
+    a.classList = "user_li registered";
+    a.setAttribute("onclick", "return handleLogout()");
+    a.textContent = "Logout";
+    li.appendChild(a);
 
-  //login
-  loginForm.addEventListener("submit", (event) => {
-    event.preventDefault();
+    const li2 = document.createElement("li");
+    const a2 = document.createElement("a");
+    a2.setAttribute("href", "./user.html");
+    a2.textContent = `${user.username}`;
+    li2.appendChild(a2);
+
+    headerMenu.appendChild(li2);
+    headerMenu.appendChild(li);
+  }
+};
+
+const handleLogout = () => {
+  if (confirm("Are you sure you want to log out?")) {
+    fetch("http://localhost:3001/logout", {
+      method: "GET",
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+    })
+      .then((response) => {
+        // response.json();
+        document.cookie =
+          "user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        localStorage.removeItem("user");
+        console.log("User logged out!");
+        user = "";
+      })
+      .then(
+        () =>
+          (location.href =
+            "http://127.0.0.1:5555/uu-assignment-3/client/index.html")
+      )
+      .catch((error) => console.error(error));
+  }
+};
+
+const handleLogin = () => {
+  if (user == null) {
     const formData = new FormData(loginForm);
     const data = Object.fromEntries(formData.entries());
     console.log("login: ", data);
 
-    fetch("http://localhost:3001/users/login", {
+    fetch("http://localhost:3001/login", {
       method: "POST",
+      // mode: "no-cors",
       headers: {
         "Content-Type": "application/json;charset=UTF-8",
       },
@@ -22,15 +79,16 @@ window.onload = () => {
     })
       .then((response) => response.json())
       .then((data) => {
-        user = data;
-        console.log("user:", user);
-        const userElement = document.querySelector("section");
-        userElement.innerHTML = `
-      <h2>Welcome ${data.name}</h2>
-      <p>Email: ${data.email}</p>
-      <p>Address: ${data.address}</p>
-      <p>Order History: ${data.order_history}</p>
-    `;
+        if (data) {
+          let userString = JSON.stringify(data);
+          localStorage.setItem("user", userString);
+          window.location.href =
+            "http://127.0.0.1:5555/uu-assignment-3/client/index.html";
+          alert(`Logged in.\nWelcome ${data.name}`);
+          formData = "";
+        } else {
+          throw new Error("Network response was not ok");
+        }
       })
       .catch((error) => console.error(error));
 
@@ -38,9 +96,62 @@ window.onload = () => {
     const decodedValue = decodeURIComponent(cookieValue);
     const jsonObject = JSON.parse(decodedValue);
     console.log(jsonObject);
+  } else {
+    alert("User already logged in");
+  }
+};
+
+if (loginButton) {
+  loginButton.addEventListener("click", (e) => {
+    handleLogin(e);
+  });
+}
+
+//window load
+window.onload = () => {
+  isLoggedUser(user);
+
+  //logout
+  userLi.addEventListener("click", () => {
+    if (user.name) {
+      handleLogout();
+    }
   });
 
+  // //login
+
+  // loginForm.addEventListener("submit", () => {
+  //   // event.preventDefault();
+
+  //   console.log("handleLogin");
+  //   const formData = new FormData(loginForm);
+  //   const data = Object.fromEntries(formData.entries());
+  //   console.log("login: ", data);
+
+  //   fetch("http://localhost:3001/login", {
+  //     method: "POST",
+  //     // mode: "no-cors",
+  //     headers: {
+  //       "Content-Type": "application/json;charset=UTF-8",
+  //     },
+  //     body: JSON.stringify(data),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       if (data) {
+  //         let userString = JSON.stringify(data);
+  //         localStorage.setItem("user", userString);
+  //         alert(`Logged in.\nWelcome ${data.name}`);
+  //       } else {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //     })
+  //     .then(() => (window.location.href = "./index.html"))
+  //     .catch((error) => console.error(error));
+  // });
+
   //register
+
   registerForm.addEventListener("submit", (event) => {
     if (registerForm) {
       event.preventDefault();
@@ -71,7 +182,3 @@ window.onload = () => {
     }
   });
 };
-
-window.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM fully loaded and parsed");
-});
